1f8d68d7f6a2ad39e84ac37317cb3b93378bb2e7
davimafraa <davimafraaa@gmail.com>
2024-04-28 21:34:02 -0300

    Adding images

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -7,15 +7,15 @@ function Slider() {
                 <h1 className="neck-text">SELECTED PROJECTS —</h1>
             </div>
             <div id="sc-2half">
-                <img className="item" src="./images/grid1.jpeg" alt="Projeto 1"/>
-                <img className="item" src="./images/grid2.jpeg" alt="Projeto 2"/>
-                <img className="item" src="./images/grid3.jpeg" alt="Projeto 3"/>
-                <img className="item" src="./images/grid4.jpeg" alt="Projeto 4"/>
-                <img className="item" src="./images/grid5.jpeg" alt="Projeto 5"/>
-                <img className="item" src="./images/grid6.jpeg" alt="Projeto 6"/>
-                <img className="item" src="./images/grid7.jpeg" alt="Projeto 7"/>
-                <img className="item" src="./images/grid8.jpeg" alt="Projeto 8"/>
-                <img className="item" src="./images/grid9.jpeg" alt="Projeto 9"/>
+                <img className="item" src="/public/grid1.jpeg" alt="Projeto 1"/>
+                <img className="item" src="/public/grid2.jpeg" alt="Projeto 2"/>
+                <img className="item" src="/public/grid3.jpeg" alt="Projeto 3"/>
+                <img className="item" src="/public/grid4.jpeg" alt="Projeto 4"/>
+                <img className="item" src="/public/grid5.jpeg" alt="Projeto 5"/>
+                <img className="item" src="/public/grid6.jpeg" alt="Projeto 6"/>
+                <img className="item" src="/public/grid7.jpeg" alt="Projeto 7"/>
+                <img className="item" src="/public/grid8.jpeg" alt="Projeto 8"/>
+                <img className="item" src="/public/grid9.jpeg" alt="Projeto 9"/>
             </div>
             <div id="sc-3half">
                 <button id="left-arrow" className="controls">◀</button>
