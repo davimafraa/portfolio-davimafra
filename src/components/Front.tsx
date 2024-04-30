98dca9c66f6c0e5aedebec3b8b30db3e631787d2
davimafraa <davimafraaa@gmail.com>
2024-04-29 22:59:29 -0300

    Responsive image

--- src/components/Front.tsx
+++ src/components/Front.tsx
@@ -2,9 +2,11 @@ function Front() {
   return (
     <>
         <section id="first-container">
+          <div className="info">
             <h1>TAKE AWAY WHAT'S IN YOUR WAY</h1>
             <h2>Build astonishing websites and conquer technology before it obliterates you</h2>
             <a href="#">CONTACT ME</a>
+          </div>
         </section>
     </>
   )
